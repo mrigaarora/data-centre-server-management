b02cdc3ae2832c929ae4214fa62e28ec4366c51c
Mriga Arora <mrigaarora02@gmail.com>
2023-07-25 13:10:14 +0530

    Remove specific folder 2

--- project/qrapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('search',views.search,name='search'),
-    path('com',views.com,name="com"),
-    path('apps',views.apps,name='apps'),
-]
